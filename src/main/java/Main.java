e2c400062e64ee20084db4feceb32796167c9ee7
Aleksandr Chivarzin <aleksandr@chivarzin.ru>
2025-11-16 11:34:33 +0300

    Ввод имён автомобилей

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         System.out.println("Hello world!");
+        Automobile[] autos = new Automobile[3]; //Автомобили - участники гонки
+        // Переменные для информации об автомобиле
+        double speed;
+        String name = "";
+        // Ввод
+        for (short i = 0; i < 3; i++) {
+            name = "";
+            while (name.equals("")) {
+                System.out.println("Введите название автомобиля " + i);
+                name = in.nextLine(); //допускаем имя автомобиля с пробелами
+            }
+        }
     }
 }
