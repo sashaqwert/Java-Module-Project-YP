7a2da28364c8a4d9b7aeb77f463e2527fce0656a
Aleksandr Chivarzin <aleksandr@chivarzin.ru>
2025-11-16 12:01:28 +0300

    Main почти готов...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,10 +11,21 @@ public class Main {
         // Ввод
         for (short i = 0; i < 3; i++) {
             name = "";
-            while (name.equals("")) {
-                System.out.println("Введите название автомобиля " + i);
+            while (name.isEmpty()) {
+                System.out.println("Введите название автомобиля " + (i + 1));
                 name = in.nextLine(); //допускаем имя автомобиля с пробелами
             }
+            speed = -1.0;
+            while (!((speed >= 0) && (speed <= 250))) {
+                System.out.println("Введите скорость автомобиля " + (i + 1));
+                speed = in.nextDouble();
+            }
+            Automobile авто = new Automobile(name, speed);
+            autos[i] = авто;
         }
+        System.out.println("Ввод успешно завершён!");
+        Gonka гонка = new Gonka(autos);
+        Automobile лидер = гонка.startAndGetLeader(24);
+        System.out.println("Самая быстрая машина: " + лидер.name);
     }
 }
