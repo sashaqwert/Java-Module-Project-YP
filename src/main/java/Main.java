8d13cb83ead9426d8be9568d80c24d7ba1dffa88
Aleksandr Chivarzin <aleksandr@chivarzin.ru>
2025-11-16 12:02:37 +0300

    Удалён лишний вывод Hello world

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Hello world!");
         Automobile[] autos = new Automobile[3]; //Автомобили - участники гонки
         // Переменные для информации об автомобиле
         double speed;
