37eb9e43fe0f68ffc5f5a9d6c71f43fbd8b4239a
Aleksandr Chivarzin <aleksandr@chivarzin.ru>
2025-11-16 11:23:28 +0300

    Класс "Gonka" (гонка). Определение победителя.

--- src/main/java/Gonka.java
+++ src/main/java/Gonka.java
@@ -4,4 +4,17 @@ public class Gonka {
     public Gonka(Automobile[] autos) {
         this.autos = autos;
     }
+
+    public Automobile startAndGetLeader(int hours) {
+        double max_distance = -1.0;
+        Automobile leader = null;
+        for (Automobile a : autos) {
+            double distance = a.calculateDistance(hours);
+            if (distance > max_distance) {
+                max_distance = distance;
+                leader = a;
+            }
+        }
+        return leader;
+    }
 }
